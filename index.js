24373547a8c38e787a3a6aa5e32e1968a0d9fac0
shreys7 <shreyshah97@gmail.com>
2019-12-02 16:42:36 +0530

    Enabled eslint in the project with standard rules

--- index.js
+++ index.js
@@ -1,75 +1,75 @@
-var converter = require('./lib/convert'),
-    fs = require('fs'),
-    yaml = require('js-yaml');
+var converter = require('./lib/convert')
+var fs = require('fs')
+var yaml = require('js-yaml')
 module.exports = {
-    getOptions: function() {
-        return [];
-    },
-    convert: function(input, options, callback) {
-        let converterOptions = {
-                group: true, // Group requests into folders
-                test: false // Disable "test mode"
-            },
-            success = (collection, environment) => {
-            return callback(null, {
-                result: true,
-                output: [
-                    {
-                        type: 'collection',
-                        data: collection
-                    },
-                    {
-                        type: 'environment',
-                        data: environment
-                    }
-                ]
-            });
-            },
-            failure = (error) => {
-                return callback({
-                    result: false,
-                    reason: error
-                });
-            },
-            data;
-        if (input.type === 'file') {
-            data = fs.readFileSync(input.data).toString();
-            data = yaml.safeLoad(data);
-            if (typeof data !== 'object') {
-                failure('Data is not valid JSON/YAML.')
-            }
-            converter.convertJSON(data, converterOptions, success, failure);
-        }
-        else if(input.type === 'string') {
-            data = yaml.safeLoad(input.data);
-            if (typeof data !== 'object') {
-                failure('Data is not valid JSON/YAML.')
-            }
-            converter.convertJSON(data, converterOptions, success, failure);
-        }
-        else {
-            return callback({
-                result: false,
-                reason: 'input type: ' + input.type + ' is not valid'
-            });
-        }
-    },
-    validate: function(input) {
-        let data;
-        if (input.type === 'file') {
-            data = fs.readFileSync(input.data).toString();
-            data = yaml.safeLoad(data);
-            return { result: typeof data === 'object' && data.hasOwnProperty('swaggerVersion') };
-        }
-        else if(input.type === 'string') {
-            data = yaml.safeLoad(input.data);
-            return { result: typeof data === 'object' && data.hasOwnProperty('swaggerVersion') };
-        }
-        else {
-            return { 
-                result: false,
-                reason: 'input type: ' + input.type + ' is not valid'
-            };
-        }
+  getOptions: function () {
+    return []
+  },
+  convert: function (input, options, callback) {
+    const converterOptions = {
+      group: true, // Group requests into folders
+      test: false // Disable "test mode"
     }
+    const success = (collection, environment) => {
+      return callback(null, {
+        result: true,
+        output: [
+          {
+            type: 'collection',
+            data: collection
+          },
+          {
+            type: 'environment',
+            data: environment
+          }
+        ]
+      })
+    }
+    const failure = (error) => {
+      // eslint-disable-next-line standard/no-callback-literal
+      return callback({
+        result: false,
+        reason: error
+      })
+    }
+    let data
+    if (input.type === 'file') {
+      data = fs.readFileSync(input.data).toString()
+      data = yaml.safeLoad(data)
+      if (typeof data !== 'object') {
+        failure('Data is not valid JSON/YAML.')
+      }
+      converter.convertJSON(data, converterOptions, success, failure)
+    } else if (input.type === 'string') {
+      data = yaml.safeLoad(input.data)
+      if (typeof data !== 'object') {
+        failure('Data is not valid JSON/YAML.')
+      }
+      converter.convertJSON(data, converterOptions, success, failure)
+    } else {
+      // eslint-disable-next-line standard/no-callback-literal
+      return callback({
+        result: false,
+        reason: 'input type: ' + input.type + ' is not valid'
+      })
+    }
+  },
+  validate: function (input) {
+    let data
+    if (input.type === 'file') {
+      data = fs.readFileSync(input.data).toString()
+      data = yaml.safeLoad(data)
+      // eslint-disable-next-line no-prototype-builtins
+      return { result: typeof data === 'object' && data.hasOwnProperty('swaggerVersion') }
+    } else if (input.type === 'string') {
+      data = yaml.safeLoad(input.data)
+      // eslint-disable-next-line no-prototype-builtins
+      return { result: typeof data === 'object' && data.hasOwnProperty('swaggerVersion') }
+    } else {
+      return {
+        result: false,
+        reason: 'input type: ' + input.type + ' is not valid'
+      }
+    }
+  }
 }
